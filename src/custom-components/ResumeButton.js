50fdf0addaec2cfb5d262ef302a325f96fe92245
JasonXB <jasonxportfolio@gmail.com>
2022-06-13 05:37:04 -0400

    The resume button now links to a hosted PDF on drive

--- src/custom-components/ResumeButton.js
+++ src/custom-components/ResumeButton.js
@@ -4,26 +4,29 @@ import Button from "@mui/material/Button";
 export default function ResumeButton({ customVariant }) {
   if (!customVariant)
     return (
-      <Button variant="outlined" color="primary">
-        Resume
-      </Button>
+      <a href="https://drive.google.com/file/d/1ogbfaF6RY__suRVBi_6oFM8g3mSmFAPV/view">
+        <Button variant="outlined" color="primary">
+          Resume
+        </Button>
+      </a>
     );
   else
     return (
-      <Button
-        variant="outlined"
-        color="info"
-        sx={{
-          width: "50%",
-          mx: "auto",
-          color: "white",
-          borderColor: "white",
-          mt: 2,
-          mb: 2,
-          ["@media (max-width: 400px)"]: { width: "100%"}
-        }}
-      >
-        Resume Download
-      </Button>
+      <a style={{margin: "auto"}} href="https://drive.google.com/file/d/1ogbfaF6RY__suRVBi_6oFM8g3mSmFAPV/view">
+        <Button
+          variant="outlined"
+          color="info"
+          sx={{
+            px: 4,
+            color: "white",
+            borderColor: "white",
+            mt: 2,
+            mb: 2,
+            ["@media (max-width: 400px)"]: { width: "100%" },
+          }}
+        >
+          Resume Download
+        </Button>
+      </a>
     );
 }
